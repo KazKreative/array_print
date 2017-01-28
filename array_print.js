ad8ed71fc5f9a7549651a78e55a7817a59bfb98c
Jonathan Kaz <contact@redstarfilmsnc.com>
2017-01-28 12:28:06 -0500

    initial commit

---
+++ array_print.js
@@ -0,0 +1,14 @@
+//	write some code that prints out each of the elements in the array.
+
+// ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p']
+
+
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'];
+
+for(var i = 0; i < letters.length; i++) {
+ console.log("kaz " + letters[i]);
+}
+
+
+//given that same array, 
+//write some code that only prints out every 2nd element in the array
